b881e29385eea3324e19db8294f4fac7ac2bc63c
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-04 13:47:33 -0300

    Simplificação do If/Else - Dark Mode/Light Mode

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,8 +6,7 @@ const rootHtml = document.documentElement;
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
-    else rootHtml.setAttribute("data-theme", "dark");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
