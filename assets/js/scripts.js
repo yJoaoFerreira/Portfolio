059e6ab68771e32e2e7cd06d55b649a21bce9c3a
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-04 13:36:42 -0300

    light mode implementado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,11 @@
 const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
 
 function changeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
+
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 };
