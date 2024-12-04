d986242631b0858877175239e3b7baa2484aa21a
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-04 13:46:38 -0300

    Finish Light and Dark

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+// Dark Mode & Light Mode
+
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 
@@ -5,6 +7,7 @@ function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
     if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
+    else rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
